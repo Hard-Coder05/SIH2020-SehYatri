83d38135bbd2db9230b9ba75f0eb8534b19be1c7
sahil9001 <sahilsilare@gmail.com>
2020-08-01 18:36:42 +0530

    Added gitignore

--- backend/users/models.py
+++ backend/users/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     route_slug = models.SlugField(max_length=40)
-    name = models.CharField(max_length=30, null=False, blank=False)
+    name = models.CharField(max_length=30, null=False, blank=True)
     phone = models.CharField(max_length=20,blank=True,null=True)
     email = models.CharField(max_length=50)
 
